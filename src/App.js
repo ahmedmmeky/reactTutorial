f2bcb47f05fa4963a58983333cf766dedba20f81
ahmedmmeky <meky.wrk@gmail.com>
2021-08-01 17:05:44 -0400

    went over the basics of the useRef hook.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import UseStateArray from "./tutorial/1-useState/setup/3-useState-array";
-import Setup from "./tutorial/4-forms/setup/2-multiple-inputs";
+import Setup from "./tutorial/5-useRef/setup/1-useRef-basics";
 function App() {
   return (
     <div className="container">
