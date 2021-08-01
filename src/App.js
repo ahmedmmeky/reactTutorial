e7d331fcd378ea58b967baa6d01480ac0b1115f9
ahmedmmeky <meky.wrk@gmail.com>
2021-08-01 16:58:05 -0400

    went over the forms tutorial in react.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import UseStateArray from "./tutorial/1-useState/setup/3-useState-array";
-import Setup from "./tutorial/3-conditional-rendering/setup/3-show-hide";
+import Setup from "./tutorial/4-forms/setup/2-multiple-inputs";
 function App() {
   return (
     <div className="container">
