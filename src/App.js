c61ba290198436a1088d281c276e55ae0906b559
ahmedmmeky <meky.wrk@gmail.com>
2021-08-01 19:03:53 -0400

    completed the custom hooks tutorial.

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 import UseStateArray from "./tutorial/1-useState/setup/3-useState-array";
-import Setup from "./tutorial/8-useContext/setup/1-context-api";
+import Setup from "./tutorial/9-custom-hooks/setup/1-fetch-example";
 function App() {
   return (
     <div className="container">
