c9a2e413873396bd35788c29c869e3747267d718
ahmedmmeky <meky.wrk@gmail.com>
2021-08-01 17:46:38 -0400

    finished the useReducer tutorial.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import UseStateArray from "./tutorial/1-useState/setup/3-useState-array";
-import Setup from "./tutorial/5-useRef/setup/1-useRef-basics";
+import Setup from "./tutorial/6-useReducer/setup";
 function App() {
   return (
     <div className="container">
