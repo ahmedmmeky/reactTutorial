1518fa7b1a0d5665f571e9b9d6fecc630ae2baf1
ahmedmmeky <meky.wrk@gmail.com>
2021-08-01 19:22:00 -0400

    learned about props in the prop tutorial.

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 import UseStateArray from "./tutorial/1-useState/setup/3-useState-array";
-import Setup from "./tutorial/9-custom-hooks/setup/1-fetch-example";
+import Setup from "./tutorial/10-prop-types/setup";
 function App() {
   return (
     <div className="container">
--- src/tutorial/10-prop-types/setup/Product.js
+++ src/tutorial/10-prop-types/setup/Product.js
@@ -1,7 +1,30 @@
-import React from 'react';
-
-const Product = () => {
-  return <article className='product'>single product</article>;
+import React from "react";
+import PropTypes from "prop-types";
+import defaultImage from "../../../assets/default-image.jpeg";
+////this could cause erorrs if a single property on a single object doesn't exist
+const Product = ({ image, name, price }) => {
+  const url = image && image.url;
+  return (
+    <article className="product">
+      <h4>single product</h4>
+      <img src={url || defaultImage} alt={name} />
+      <h4>{name}</h4>
+      <p>${price || 3.99}</p>
+    </article>
+  );
 };
+
+Product.propTypes = {
+  image: PropTypes.object.isRequired,
+  name: PropTypes.string.isRequired,
+  price: PropTypes.number.isRequired,
+};
+
+/* use this in case one of the properties are not there 
+Product.defaultProps = {
+  name: "default name",
+  price: 3.99,
+  image: defaultImage,
+};*/
 
 export default Product;
