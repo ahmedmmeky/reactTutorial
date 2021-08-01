601bd27a4343c7d99ad2fc9eef594716561f1979
ahmedmmeky <meky.wrk@gmail.com>
2021-08-01 18:43:15 -0400

    went over the concept of prop drilling.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
+
 import UseStateArray from "./tutorial/1-useState/setup/3-useState-array";
-import Setup from "./tutorial/6-useReducer/setup";
+import Setup from "./tutorial/7-prop-drilling/setup/1-prop-drilling";
 function App() {
   return (
     <div className="container">
